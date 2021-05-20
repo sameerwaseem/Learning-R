7f2ae6615c3c18a165d71a235abe76e035ab9e90
Sameer <asameer10@gmail.com>
2021-05-21 01:57:39 +0500

    Added few lines

--- Testing.r
+++ Testing.r
@@ -1,3 +1,11 @@
 # This is from Github
 print("Hello from github")
 
+# This is from Github
+print("Hello from github")
+
+name = "Sameer"
+version = "R-STUDIO - In writing"
+
+print(name)
+print("this is written in r-studio")
